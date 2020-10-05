073e5530928acc6b1f93758c33c72c70b7d1f1c2
Kunjan Dalal <kunjee17@gmail.com>
2020-10-05 08:35:58 +0530

    Aggregate tests are working

--- cosmo_store_util/src/lib.rs
+++ cosmo_store_util/src/lib.rs
@@ -1,7 +1,146 @@
+use std::fmt::{Debug, Display};
+
+pub struct Aggregate<State, Command, Event> {
+    pub init: State,
+    pub apply: fn(state: &State, event: &Event) -> State,
+    pub execute: fn(state: &State, command: &Command) -> Vec<Event>,
+}
+
 #[cfg(test)]
 mod tests {
+    use chrono::NaiveDateTime;
+    use uuid::Uuid;
+    use crate::Aggregate;
+
+    #[derive(Clone, Debug)]
+    pub struct AddTodo {
+        pub id: Uuid,
+        pub name: String,
+        pub is_complete: bool,
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct RemoveTodo {
+        pub id: Uuid,
+    }
+
+    #[derive(Clone, Debug)]
+    pub struct CompleteTodo {
+        pub id: Uuid
+    }
+
+    #[derive(Clone, Debug)]
+    pub enum TodoCommand {
+        AddTodo(AddTodo),
+        RemoveTodo(RemoveTodo),
+        ClearAllTodo,
+        CompleteTodo(CompleteTodo)
+    }
+
+    #[derive(Clone, Debug)]
+    pub enum TodoEvent {
+        TodoAdded(AddTodo),
+        TodoRemoved(RemoveTodo),
+        AllTodoCleared,
+        TodoCompleted(CompleteTodo)
+    }
+
+    #[derive(Clone, Debug)]
+    struct Todo {
+        id: Uuid,
+        name: String,
+        is_completed : bool
+    }
+
+    #[derive(Clone, Debug)]
+    struct TodoState {
+        todos : Vec<Todo>
+    }
+
+    impl TodoState {
+        pub const fn init() -> TodoState {
+            TodoState {
+                todos: vec![]
+            }
+        }
+    }
+
+    const TODO_AGGREGATE: Aggregate<TodoState, TodoCommand, TodoEvent> =  Aggregate {
+        init: TodoState::init(),
+        apply: |state, event| {
+            match event {
+                TodoEvent::TodoAdded(t) => {
+                    let new_state = Todo {
+                        id: t.id,
+                        name: t.name.clone(),
+                        is_completed: t.is_complete
+                    };
+                    TodoState {
+                        todos: state.clone().todos.into_iter().chain(vec![new_state]).collect()
+                    }
+                }
+                TodoEvent::TodoRemoved(t) => {
+                    let filtered: Vec<Todo> = state.clone().todos.into_iter().filter(|p| p.id != t.id).collect();
+                    TodoState {
+                        todos: filtered
+                    }
+                }
+                TodoEvent::AllTodoCleared => {
+                    TodoState {
+                        todos: vec![]
+                    }
+                }
+                TodoEvent::TodoCompleted(t) => {
+                    let find_todo: Todo = state.todos.iter().find(|p| p.id == t.id).unwrap().clone();
+                    let filtered: Vec<Todo> = state.clone().todos.into_iter().filter(|p| p.id != t.id).collect();
+                    TodoState {
+                        todos:  filtered.into_iter().chain(vec![Todo{ is_completed: true, ..find_todo }]).collect()
+                    }
+                }
+            }
+        },
+        execute: |state , command| {
+            match command {
+                TodoCommand::AddTodo(t) => {
+                    vec![TodoEvent::TodoAdded(t.clone())]
+                }
+                TodoCommand::RemoveTodo(t) => {
+                    vec![TodoEvent::TodoRemoved(t.clone())]
+                }
+                TodoCommand::ClearAllTodo => {
+                    vec![TodoEvent::AllTodoCleared]
+                }
+                TodoCommand::CompleteTodo(t) => {
+                    vec![TodoEvent::TodoCompleted(t.clone())]
+                }
+            }
+        }
+    };
+
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn init_state() {
+        assert_eq!(TODO_AGGREGATE.init.todos.len(), TodoState::init().todos.len())
+    }
+
+    #[test]
+    fn add_state() {
+        let events = (TODO_AGGREGATE.execute)(&TODO_AGGREGATE.init, &TodoCommand::AddTodo(AddTodo{
+            id: Default::default(),
+            name: "Some Task".to_string(),
+            is_complete: false
+        }));
+
+        assert_eq!(events.len(), 1);
+
+        let state = events.iter().fold(TODO_AGGREGATE.init, |a, b| (TODO_AGGREGATE.apply)(&a, &b));
+
+        assert_eq!(state.todos.len(), 1);
+    }
+
 }
